b936a080bf77340472ba95a7fd5256c7057dd022
User00morph <emoefe1838@gmail.com>
2026-01-18 18:24:39 -0600

    Create morph-lunar-calendar.js

---
+++ morph-lunar-calendar.js
@@ -0,0 +1,11 @@
+{
+  "name": "morph-lunar",
+  "version": "1.0.0",
+  "main": "morph-lunar-calendar.js",
+  "scripts": {
+    "build": "node morph-lunar-calendar.js"
+  },
+  "dependencies": {
+    "luxon": "^3.0.0"
+  }
+}
